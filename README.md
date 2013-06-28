72180333a226f67395e46063139eab6a63b10c12
matlecu <mathias.lecuyer@gmail.com>
2013-06-28 16:45:12 -0400

    added popup with credentials and fork me link

---
+++ README.md
@@ -0,0 +1,1 @@
+Timelog - enhance your Rally timesheet
--- src/background/eventbackground.js
+++ src/background/eventbackground.js
@@ -1,13 +1,6 @@
 var tabId;
 var data = [];
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  tabId = tab.id;
-  chrome.tabs.executeScript(tab.id, { code: "if (typeof(addTimerToSelected) === 'function') {var x = addTimerToSelected(); x}", allFrames: true }, function (results) {
-    console.log(results);});
-}
-);
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.query == "data") {
     sendResponse({data: data});
