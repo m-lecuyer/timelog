d05a7ef425202942e341aee077a24925692d1320
matlecu <mathias.lecuyer@gmail.com>
2013-06-29 12:58:28 -0400

    added persistance of time info in chrome storage

--- src/background/eventbackground.js
+++ src/background/eventbackground.js
@@ -3,8 +3,13 @@ var data = [];
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.query == "data") {
-    sendResponse({data: data});
+    chrome.storage.sync.get(function(data) {
+      sendResponse({data: data});
+    });
+    return true;  // to allow async answer
   } else if (request.query == "save") {
+    if (data != request.data)
+      chrome.storage.sync.set(request.data);
     data = request.data;
   }
 });
